8748611b95927e983405d1af61fef92625f72f91
KonstantinVolodko <faker2000311@gmail.com>
2023-11-24 20:46:42 +0300

    add animations and fixes

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -30,6 +30,25 @@ document.addEventListener("DOMContentLoaded", () => {
     touchpadSupport: true,
   })
 
+  let prevScrollPosition = window.pageYOffset;
+  let header = document.querySelector('header');
+  let headerHeight = header.offsetHeight;
+  let page = document.querySelector('.page')
+  page.style.paddingTop = `${headerHeight + 'px'}`
+
+
+  window.onscroll = function () {
+    let currentScrollPosition = window.pageYOffset;
+
+    if (prevScrollPosition > currentScrollPosition) {
+      header.style.top = "0";
+    } else {
+      header.style.top = `-${headerHeight + 'px'}`;
+    }
+
+    prevScrollPosition = currentScrollPosition;
+  }
+
 
   function typeAnimation(event) {
     var textElement = document.querySelector(".title-block__title h1");
@@ -168,10 +187,10 @@ document.addEventListener("DOMContentLoaded", () => {
       end: "bottom top",
       toggleActions: "restart none none none"
     },
-    duration: 1, 
-    opacity: 0,   
-    y: 30,         
-    stagger: 0.2,  
+    duration: 1,
+    opacity: 0,
+    y: 30,
+    stagger: 0.2,
     ease: "power2.out"
   });
 
@@ -211,7 +230,7 @@ document.addEventListener("DOMContentLoaded", () => {
   gsap.from(".addition ul li", {
     scrollTrigger: {
       trigger: ".addition ul",
-      start: "top bottom", // Начало анимации, когда верх списка достигает нижней части вьюпорта
+      start: "top 60%", // Начало анимации, когда верх списка достигает нижней части вьюпорта
       end: "bottom top",
       toggleActions: "play none none none"
     },
@@ -254,7 +273,7 @@ document.addEventListener("DOMContentLoaded", () => {
   gsap.from(".technical-capabilities-swiper .swiper-slide", {
     scrollTrigger: {
       trigger: ".technical-capabilities__content",
-      start: "top bottom", // Начало анимации, когда верх списка достигает нижней части вьюпорта
+      start: "top 70%", // Начало анимации, когда верх списка достигает нижней части вьюпорта
       end: "bottom top",
       toggleActions: "play none none none"
     },
@@ -268,7 +287,7 @@ document.addEventListener("DOMContentLoaded", () => {
   gsap.from(".contacts__inner", {
     scrollTrigger: {
       trigger: ".contacts",
-      start: "top bottom",
+      start: "top 50%",
       toggleActions: "play none none none"
     },
     x: -200, // начальное смещение по горизонтали
@@ -276,18 +295,135 @@ document.addEventListener("DOMContentLoaded", () => {
     duration: 1,
     ease: "power3.out"
   });
-  
+
   // Анимация для блока с формой
   gsap.from(".contacts__content form", {
     scrollTrigger: {
       trigger: ".contacts",
-      start: "top bottom",
+      start: "top 50%",
       toggleActions: "play none none none"
     },
     x: 200, // начальное смещение по горизонтали
     opacity: 0,
     duration: 1,
+    delay: 0.4,
     ease: "power3.out"
   });
 
+  // gsap.from(".innovations ul li", {
+  //   opacity: 0,
+  //   y: 50,
+  //   stagger: 0.3, // Задержка между анимацией каждого элемента списка
+  //   scrollTrigger: {
+  //     trigger: ".innovations__content",
+  //     start: "top 80%", // Анимация начнется, когда верх блока .innovations__content достигнет 80% высоты окна просмотра
+  //     end: "bottom top",
+  //     toggleActions: "play none none none"
+  //   }
+  // });
+
+  // gsap.from(".innovations__img", {
+  //   opacity: 0,
+  //   x: -50,
+  //   duration: 1, // Продолжительность анимации
+  //   scrollTrigger: {
+  //     trigger: ".innovations__content",
+  //     start: "top 80%",
+  //     end: "bottom top",
+  //     toggleActions: "play none none none"
+  //   }
+  // });
+
+  function getRandomDirection() {
+    var angle = Math.random() * 360;
+    return angle;
+  }
+
+  function getRandomVelocity() {
+    var minVelocity = 1;
+    var maxVelocity = 3;
+    var velocity = Math.random() * (maxVelocity - minVelocity) + minVelocity;
+    return velocity;
+  }
+
+  function checkCollision(element, otherElements) {
+    var elementRect = element.getBoundingClientRect();
+
+    for (var i = 0; i < otherElements.length; i++) {
+      var otherRect = otherElements[i].getBoundingClientRect();
+
+      if (
+        element !== otherElements[i] &&
+        elementRect.right >= otherRect.left &&
+        elementRect.left <= otherRect.right &&
+        elementRect.bottom >= otherRect.top &&
+        elementRect.top <= otherRect.bottom
+      ) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  function animateElement(containerSelector, elementSelector) {
+    var containers = document.querySelectorAll(containerSelector);
+    console.log(containers)
+    if (!containers.length) {
+      console.warn(`No containers found for ${containerSelector}.`);
+      return;
+    }
+
+    containers.forEach(function (container) {
+      var elements = container.querySelectorAll(elementSelector);
+      if (!elements.length) {
+        console.warn(`No elements found for ${elementSelector} inside ${containerSelector}.`);
+        return;
+      }
+
+      elements.forEach(function (element) {
+        var containerRect = container.getBoundingClientRect();
+        var elementRect = element.getBoundingClientRect();
+
+        var containerWidth = containerRect.width - elementRect.width;
+        var containerHeight = containerRect.height - elementRect.height;
+
+        var posX = 0;
+        var posY = 0;
+        var direction = getRandomDirection();
+        var velocity = getRandomVelocity();
+        var isColliding = false;
+
+        var animationInterval = setInterval(function () {
+          var radians = direction * Math.PI / 180;
+          var deltaX = velocity * Math.cos(radians);
+          var deltaY = velocity * Math.sin(radians);
+
+          posX += deltaX;
+          posY += deltaY;
+
+          if (posX < 0 || posX > containerWidth) {
+            direction = (180 - direction) % 360;
+          }
+          if (posY < 0 || posY > containerHeight) {
+            direction = (360 - direction) % 360;
+          }
+
+          if (checkCollision(element, elements)) {
+            if (!isColliding) {
+              direction = (direction + 180) % 360;
+            }
+            isColliding = true;
+          } else {
+            isColliding = false;
+          }
+
+          element.style.transform = `translate(${posX}px, ${posY}px)`;
+        }, 30);
+      });
+    });
+  }
+
+  animateElement('.why-us .rotation-card', '.rotation-card__background');
+  animateElement('.innovations', '.innovations__background');
 })
